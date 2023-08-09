bb2686bd0ac3a687507a64b351ea2f06eac3d9b6
byungjun0689 <>
2023-08-09 13:12:34 +0900

    DynamoDB Create Table and Import Data, Kinesis_stream_create and generate stream data

---
+++ set-up-streaming-hands-on.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -
+
+OS_NAME=$(cat /etc/os-release | awk -F "=" '$1 == "NAME" { print $2 }')
+if [[ z"${OS_NAME}" == z"\"Amazon Linux AMI\"" ]];
+then
+  sudo yum -y update
+  sudo yum -y install python36
+  sudo yum -y install python3-pip
+
+  sudo pip-3.6 install -U boto3
+  sudo pip-3.6 install csvkit
+  sudo pip-3.6 install mimesis==4.1.3
+  sudo pip3 install pandas
+elif [[ z"${OS_NAME}" == z"\"Amazon Linux\"" ]];
+then
+  sudo yum -y update
+  sudo yum -y install python3
+  sudo yum -y install python3-pip
+
+  sudo pip3 install -U boto3
+  sudo pip3 install csvkit
+  sudo pip3 install mimesis==4.1.3
+  sudo pip3 install pandas
+elif [[ z"${OS_NAME}" == z"\"Ubuntu\"" ]];
+then
+  sudo apt-get -y update
+  sudo apt-get -y install python3.6
+  sudo apt-get -y install python3-pip
+
+  sudo pip3 install -U boto3
+  sudo pip3 install csvkit
+  sudo pip3 install mimesis==4.1.3
+  sudo pip3 install pandas
+else
+  echo "[Unknown OS] You should install python3.6+, pip3+ for yourself!"
+  exit 0
+fi
