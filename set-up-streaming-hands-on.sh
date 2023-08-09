7353258d0562006111065a307b81b69ae8e29b7a
byungjun0689 <>
2023-08-09 14:38:23 +0900

    change default ec2 type

--- set-up-streaming-hands-on.sh
+++ set-up-streaming-hands-on.sh
@@ -4,12 +4,12 @@ OS_NAME=$(cat /etc/os-release | awk -F "=" '$1 == "NAME" { print $2 }')
 if [[ z"${OS_NAME}" == z"\"Amazon Linux AMI\"" ]];
 then
   sudo yum -y update
-  sudo yum -y install python36
+  sudo yum -y install python39
   sudo yum -y install python3-pip
 
-  sudo pip-3.6 install -U boto3
-  sudo pip-3.6 install csvkit
-  sudo pip-3.6 install mimesis==4.1.3
+  sudo pip-3.9 install -U boto3
+  sudo pip-3.9 install csvkit
+  sudo pip-3.9 install mimesis==4.1.3
   sudo pip3 install pandas
 elif [[ z"${OS_NAME}" == z"\"Amazon Linux\"" ]];
 then
@@ -24,7 +24,7 @@ then
 elif [[ z"${OS_NAME}" == z"\"Ubuntu\"" ]];
 then
   sudo apt-get -y update
-  sudo apt-get -y install python3.6
+  sudo apt-get -y install python3.9
   sudo apt-get -y install python3-pip
 
   sudo pip3 install -U boto3
